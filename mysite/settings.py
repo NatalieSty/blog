cb46614e3a5df06cae2e69774dbfc3cd4dbe181b
Natalie Tsz Yui So <natalie_soo@hotmail.com>
2020-03-26 02:26:15 -0700

    Edited css styling

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'hzetdi)f#0okx$zu5y=7aae$9e6q25#+wmlu%)#=1s9h-m5s&_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nataliesotszyui.pythonanywhere.com']
-
+#ALLOWED_HOSTS = ["nataliesotszyui.pythonanywhere.com", '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
